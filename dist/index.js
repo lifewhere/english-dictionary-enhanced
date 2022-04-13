cc4cea71ce22d55caa5bbcc1e5c780bedd106ecf
lifewhere <167460495+lifewhere@users.noreply.github.com>
2022-04-13 11:13:48 +0000

    - пересоздание бранча

--- dist/index.js
+++ dist/index.js
@@ -417,3 +417,45 @@ class Dictionary {
           return false;
         }
         lemmaSplit.splice(found, 1);
+      }
+      return true;
+    }).map((item) => item.lemma);
+    return output;
+  }
+  wordsWithCharsIn(query, priorityCharacters = "") {
+    const matchingWords = this.database.index.filter((item) => Dictionary.hasAllCharsIn(query, item.lemma)).map((item) => item.lemma).sort((a, b) => {
+      let diff = 0;
+      if (priorityCharacters.length > 0) {
+        const aPriority = Dictionary.hasAllCharsIn(a, priorityCharacters) ? 10 : 0;
+        const bPriority = Dictionary.hasAllCharsIn(b, priorityCharacters) ? 10 : 0;
+        diff = b.length + bPriority - (a.length + aPriority);
+      } else {
+        diff = b.length - a.length;
+      }
+      return diff;
+    }).splice(0, 10);
+    return this.searchSimpleFor(matchingWords);
+  }
+  static hasAllCharsIn(word, test) {
+    const wordSplit = word.split("").sort();
+    const testSplit = test.split("").sort();
+    if (testSplit.length > wordSplit.length) {
+      return false;
+    }
+    for (let i = 0;i < testSplit.length; i += 1) {
+      const foundChar = wordSplit.indexOf(testSplit[i]);
+      if (foundChar < 0) {
+        return false;
+      }
+      wordSplit.splice(foundChar, 1);
+    }
+    return true;
+  }
+}
+var dictionary_default = Dictionary;
+
+// src/index.ts
+var src_default = dictionary_default;
+export {
+  src_default as default
+};
