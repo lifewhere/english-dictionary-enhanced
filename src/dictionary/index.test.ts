34fbf96703eaf064a6066915abcda9e3e1b82275
lifewhere <167460495+lifewhere@users.noreply.github.com>
2023-02-25 21:19:37 +0000

    Applied quick fixes.

--- src/dictionary/index.test.ts
+++ src/dictionary/index.test.ts
@@ -1,1 +1,3 @@
-import { expect, test, describe, beforeAll } from "bun:test"
+import { expect, test, describe, beforeAll } from "bun:test";
+const wordnet = require("en-wordnet").default;
+import Dictionary from "../dictiona
