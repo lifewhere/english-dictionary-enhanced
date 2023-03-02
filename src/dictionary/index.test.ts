18eccaa605d085ae7d7bfaa990db246e25bb84c6
lifewhere <167460495+lifewhere@users.noreply.github.com>
2023-03-02 17:10:30 +0000

    compile fix.

--- src/dictionary/index.test.ts
+++ src/dictionary/index.test.ts
@@ -4,4 +4,9 @@ import Dictionary from "../dictionary";
 
 const dictionary = new Dictionary(wordnet.get("3.0")!);
 
-describe("Test the dictio
+describe("Test the dictionary", () => {
+  beforeAll(async () => {
+    await dictionary.init();
+  }, 20000);
+
+  test("Test searchWord", 
