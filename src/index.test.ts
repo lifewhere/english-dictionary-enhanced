3b49d3acc8b126f7a8befe8cde0144da0e359e4a
lifewhere <167460495+lifewhere@users.noreply.github.com>
2023-06-26 12:13:39 +0000

    unlicense

--- src/index.test.ts
+++ src/index.test.ts
@@ -2,4 +2,6 @@ const enWordnet = require("en-wordnet").default;
 import { expect, test, describe, beforeAll } from "bun:test";
 import Dictionary from "./index";
 
-const dictionary = n
+const dictionary = new Dictionary(enWordnet.get("3.0"));
+
+describe("Test the index file for 
