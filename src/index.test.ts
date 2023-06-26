4a7ab9bd296c3ee3c8c143d33deba5f95798f799
lifewhere <167460495+lifewhere@users.noreply.github.com>
2023-06-26 10:57:27 +0000

    correct gitignore

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,2 +1,5 @@
 const enWordnet = require("en-wordnet").default;
-import { expect, test, describe, bef
+import { expect, test, describe, beforeAll } from "bun:test";
+import Dictionary from "./index";
+
+const dictionary = n
