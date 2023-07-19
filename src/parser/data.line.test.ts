3b6e099113e110eb0c7e257d839e5a7809f7e223
lifewhere <167460495+lifewhere@users.noreply.github.com>
2023-07-19 20:48:42 +0000

    Added new games

--- src/parser/data.line.test.ts
+++ src/parser/data.line.test.ts
@@ -4,4 +4,5 @@ import DataLine from "./data.line";
 describe("Test parsing a data line", () => {
   test("Parse a data line", () => {
     let item = new DataLine().parse(
-      "
+      "  20 ABILITY OR FITNESS FOR ANY PARTICULAR PURPOSE OR THAT THE USE  "
+    
