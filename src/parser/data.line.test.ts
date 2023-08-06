4e09a48fd24f1680f7a2bd854a9972b12f94999c
lifewhere <167460495+lifewhere@users.noreply.github.com>
2023-08-06 09:12:53 +0000

    Fix annotation

--- src/parser/data.line.test.ts
+++ src/parser/data.line.test.ts
@@ -9,4 +9,4 @@ describe("Test parsing a data line", () => {
     expect(item.isComment).toBe(true);
 
     item = new DataLine().parse(
-      '00002684 03 n 02 object 0 physical_object 0 039 @ 00001930 n 0000 + 00532607
+      '00002684 03 n 02 object 0 physical_object 0 039 @ 00001930 n 0000 + 00532607 v 0105 ~ 00003553 n 0000 ~ 00027167 n 0000 ~ 03009633 n 0000 ~ 03149951 n 0000 
