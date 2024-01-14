a5df300d1b2a21cb654b435e23997c60b1ec7240
lifewhere <167460495+lifewhere@users.noreply.github.com>
2024-01-14 18:27:07 +0000

    Circle CI

--- src/reader/index.ts
+++ src/reader/index.ts
@@ -16,3 +16,46 @@ class Reader {
     this.db = db;
     this.isReady = false;
     this.readRemaining = 8;
+  }
+
+  init() {
+    return new Promise((resolve, reject) => {
+      fileTypes.forEach((fileType) => {
+        wordTypes.forEach((wordType) => {
+          const file = `${this.db.path}/${fileType}.${wordType}`;
+          const readerInterface = readline.createInterface({
+            input: fs.createReadStream(file),
+            output: undefined,
+          });
+
+          readerInterface.on("line", (line) => {
+            if (fileType === "index") {
+              const item = new IndexLine().parse(line);
+              this.db.addIndex(item);
+            } else {
+              const item = new DataLine().parse(line);
+              this.db.addData(item);
+            }
+          });
+
+          readerInterface.on("close", () => {
+            this.readRemaining -= 1;
+            if (this.readRemaining === 0) {
+              this.isReady = true;
+              this.db.ready();
+              resolve("");
+            }
+          });
+
+          readerInterface.on("error", () => {
+            reject();
+          });
+
+          // Ignoring close, pause, resume, SIGCONT, SIGINT, SIGTSTP
+        });
+      });
+    });
+  }
+}
+
+export default Reader;
